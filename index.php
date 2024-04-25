40e22321643db9eb3787450bb26c138fc508fdfa
mask1tt <mask1tt@mail.ru>
2024-04-25 19:53:25 +0300

    Finished(add upload files)

--- index.php
+++ index.php
@@ -17,14 +17,24 @@
 <div class="container">
     <div class="row">
         <div class="col-12 index">
-            <h1> Авторизируйтесь </h1>
+            <h1> Страница постов </h1>
         <?php
-        if (!isset($_COOKIE['user'])) {
+        if (!isset($_COOKIE['User'])) {
         ?>
             <a href="/registration.php"> Зарегестрируйтесь </a> или <a href="/login.php"> войдите</a>!
         <?php
         } else {
-            //подключение к бд
+            $link = mysqli_connect('127.0.0.1', 'root', '11040413', 'first');
+
+            $sql = "SELECT * FROM posts";
+            $res = mysqli_query($link, $sql);
+            if (mysqli_num_rows($res) > 0){
+                while ($post = mysqli_fetch_array($res)) {
+                    echo "<a href='/posts.php?id=" .$post["id"] . "'>" .$post['title'] ."</a><br>";
+                }
+            } else {
+                echo "Записей пока нет";
+            }
         }
         ?>
         </div>
--- profile.php
+++ profile.php
@@ -63,9 +63,10 @@
                 </h1>
             </div>
         <div class="col-7">
-            <form method="POST" action="profile.php">
+            <form class= "form_align" method="POST" action="profile.php" enctype="multipart/form-data" name="upload">
                 <input type="text" class="form" type="text" name="title" placeholder="Заголовок поста">
                 <textarea name="text" cols="30" rows="10" placeholder="Введите текст вашего поста..."></textarea>
+                <input type="file" name="file" /><br>
                 <button type="submit" class="btn_red" name="submit">Сохранить пост!</button>
             </form>
         </div>
@@ -90,6 +91,18 @@ if (isset($_POST['submit'])) {
     $sql = "INSERT INTO posts (title, main_text) VALUES ('$title', '$main_text')";
 
     if(!mysqli_query($link, $sql)) die("Не удалось добавить пост");
+}
+
+if(!empty($_FILES["file"])){
+    if (((@$_FILES["file"]["type"] == "image/gif") || (@$_FILES["file"]["type"] == "image/jpeg") || (@$_FILES["file"]["type"] == "image/jpg") || (@$_FILES["file"]["type"] == "image/pjpeg") || (@$_FILES["file"]["type"] == "image/x-png") || (@$_FILES["file"]["type"] == "image/png") ) && (@$_FILES["file"]["size"] < 102400 ))
+    {
+        move_uploaded_file($_FILES["file"]["tmp-name"], "upload/" .$_FILES["file"]["name"]);
+        echo "Load in: " . "upload/" . $_FILES["file"]["name"];
+    }
+    else
+    {
+        echo "Upload failed!";
+    }
 }
 
 ?>
